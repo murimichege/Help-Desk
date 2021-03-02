efd1eac0ae0fd26a3d4416a6fd5fe88ab8e9e9a9
murimichege <chegeeddie@gmail.com>
2021-03-02 14:45:30 +0300

    Removed the collapsible menu function

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -25,33 +25,15 @@ import "./Header.css";
 
 const Header = () => {
   
-    //create initial menuCollapse state using useState hook
-    const [menuCollapse, setMenuCollapse] = useState(false)
-
-    //create a custom function that will change menucollapse state from false to true and true to false
-  const menuIconClick = () => {
-    //condition checking to change state from true to false and vice versa
-    menuCollapse ? setMenuCollapse(false) : setMenuCollapse(true);
-  };
-
+  
   return (
     <>
       <div id="header">
           {/* collapsed props to change menu size using menucollapse state */}
-        <ProSidebar collapsed={menuCollapse}>
+        <ProSidebar>
           <SidebarHeader>
-          <div className="logotext">
-              {/* small and big change using menucollapse state */}
-              <p>{menuCollapse ? "EasyDesk" : "Easy Desk"}</p>
-            </div>
-            <div className="closemenu" onClick={menuIconClick}>
-                {/* changing menu collapse icon on click */}
-              {menuCollapse ? (
-                <FiArrowRightCircle/>
-              ) : (
-                <FiArrowLeftCircle/>
-              )}
-            </div>
+         
+            
           </SidebarHeader>
           <SidebarContent>
             <Menu iconShape="square">
