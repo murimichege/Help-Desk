817b8790f0df0573bafe1ec9da1b5da788df5c04
murimichege <chegeeddie@gmail.com>
2021-03-31 17:01:29 +0300

    Changed to react-admin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css';
-import SideBar from './components/SideBar'
-
+import {Admin} from 'react-admin'
 function App() {
+  const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+
   return (
     <div className="App">
-      <SideBar/>
+      
+      <Admin dataprovider={dataProvider}/>
     </div>
   );
 }
