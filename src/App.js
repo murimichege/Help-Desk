d974d70d014e59a54c0dabcf25b22b866511d7e3
murimichege <chegeeddie@gmail.com>
2021-04-02 12:04:31 +0300

    changing to react admin for ui

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import './App.css';
-import {Admin} from 'react-admin'
-function App() {
-  const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
-  return (
-    <div className="App">
-      
-      <Admin dataprovider={dataProvider}/>
-    </div>
-  );
-}
+
+import { Admin, Resource, ListGuesser } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+const App = () => (
+    <Admin dataProvider={dataProvider}>
+        <Resource name="users" list={ListGuesser} />
+   </Admin>
+);
 
 export default App;
