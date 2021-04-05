912ae9b0d8a515f3032b294f1eb5b402d257f6e6
murimichege <chegeeddie@gmail.com>
2021-04-05 09:08:24 +0300

    Added items to sidebar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import './App.css';
-
-
-import { Admin, Resource, ListGuesser } from 'react-admin';
+import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
-
+import {UsersList} from './components/users'
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
     <Admin dataProvider={dataProvider}>
-        <Resource name="users" list={ListGuesser} />
+        <Resource name ="Dashboard"/>
+        <Resource name="Tickets"/>
+        <Resource name="Users" list={UsersList} />
+        <Resource name="Statuses"/>
+        <Resource name="Analytics"/>
+
+
+        
    </Admin>
 );
 
