1b44495a3a5b8f0381c44fdf3e86d06fdfab8c71
murimichege <chegeeddie@gmail.com>
2021-04-10 14:42:38 +0300

    Added three ticket number view

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,42 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 import { Card, CardContent, CardHeader } from '@material-ui/core';
-import {Title} from 'react-admin'
-function Dashboard() {
-    return (
-        <Card>
-        <CardHeader title="Dashboard" />
-        <CardContent>Lorem ipsum sic dolor amet...</CardContent>
-    </Card>
-    )
-}
 
+
+function Dashboard() {
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+  }));
+  const classes = useStyles();
+
+  return (
+    <Card>
+      <CardHeader title="Dashboard"/>
+      <CardContent>
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <Paper className={classes.paper}>xs</Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper className={classes.paper}>xs</Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper className={classes.paper}>xs</Paper>
+        </Grid>
+      </Grid>
+    </div>
+    </CardContent>
+    </Card>
+  );
+}
 export default Dashboard
