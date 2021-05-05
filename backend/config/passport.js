647822b75b8fa12b2f6fb654675228bebec92881
murimichege <chegeeddie@gmail.com>
2021-05-05 14:36:24 +0300

    added jwt strategy and decoded the encrypted password

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -1,8 +1,25 @@
 const JWTStrategy = require("passport-jwt").Strategy
-const ExtractJWT = require("passport-jwt").ExtractJWT
+const ExtractJWT = require("passport-jwt").ExtractJwt
 const mongoose = require("mongoose")
 const User = mongoose.model("user")
-const keys = require("./keys")
+const keys = require("../config/keys")
 
 
 const opts = {}
+opts.jwtFromRequest = ExtractJWT.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = keys.secretOrKey;
+
+module.exports = passport => {
+    passport.use(
+        new JWTStrategy(opts, (jwtpayload, done) => {
+            User.findById(jwtpayload.id)
+            .then(user => {
+                if(user){
+                    return done(null, user)
+                }
+                return done(null, false)
+            })
+            .catch(err => console.log(err))
+        })
+    )
+}
