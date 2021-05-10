3ab1767c0541b5df132fe1daf042d739880a3d91
murimichege <chegeeddie@gmail.com>
2021-05-10 11:53:33 +0300

    export keys

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,2 +1,4 @@
-module.exports = 'mongodb+srv://EddieChege:Chege091818@cluster0.d7q0w.mongodb.net/HelpDesk?retryWrites=true&w=majority'
+module.exports = {
+mongoURI= 'mongodb+srv://EddieChege:Chege091818@cluster0.d7q0w.mongodb.net/HelpDesk?retryWrites=true&w=majority',
 secretOrKey = "qwertyuiopasdfghjklzxcvbnm123456"
+}
