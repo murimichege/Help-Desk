f2c6d9ca9e63dc67de01bae3a114f0f0578e85a9
murimichege <chegeeddie@gmail.com>
2021-05-15 13:23:34 +0300

    same keys

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module.exports =
 {
 mongoURI:'mongodb+srv://EddieChege:Chege091818@cluster0.d7q0w.mongodb.net/HelpDesk?retryWrites=true&w=majority',
@@ -6,9 +5,3 @@ secretOrKey: "secret"
 
 }
 
-=======
-module.exports = {
-mongoURI: 'mongodb+srv://EddieChege:Chege091818@cluster0.d7q0w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-secretOrKey :"secret"
-}
->>>>>>> c0563e6b19211119607dd85f4ee7753e0dfd1fb5
