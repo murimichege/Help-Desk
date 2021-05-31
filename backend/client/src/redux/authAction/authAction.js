f2cf9277902ea3cbb41375546744f92c62b00c19
murimichege <chegeeddie@gmail.com>
2021-05-31 23:00:13 +0300

    created the authaction

---
+++ backend/client/src/redux/authAction/authAction.js
@@ -0,0 +1,60 @@
+import axios from 'axios'
+import setAuthToken from '../../setAuthToken'
+import jwt_decode from 'jwt-decode'
+
+import {GET_ERRORS, SET_CURRENT_USER, USER_LOADING} from '../Actiontypes'
+
+export const RegisterUser= (history, UserData) => dispatch => {
+    axios.post('/api/users/register', UserData)
+    .then(res => history.push('/LogIn'))
+    .catch(err => {
+        dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        })
+    })
+}
+
+export const LogInUser = (UserData) => dispatch => {
+    axios.post('/api/users/LogIn',UserData)
+    .then(res => {
+        // save to local storage
+
+        const {token} = res.data
+        localStorage("jwtToken", token)
+        setAuthToken(token)
+        // decode the token
+        const decoded = jwt_decode(token)
+        dispatch(setCurrentUser(decoded))
+        .catch(err => {
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        })
+
+
+    })
+
+    }
+
+export const setCurrentUser = (decoded) => {
+    dispatch({
+        type: SET_CURRENT_USER,
+        payload: decoded
+
+    })
+}
+
+export const UserLoading = () => {
+    dispatch({
+        type: USER_LOADING  
+
+    })
+}
+
+export const LogOutUser = () => dispatch => {
+localStorage.removeItem("jwtToken")
+setAuthToken(false)
+dispatch(setCurrentUser({}))
+}
