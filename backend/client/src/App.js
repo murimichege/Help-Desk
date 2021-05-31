3899f2915e4193bbd64c522d4723f31422291247
murimichege <chegeeddie@gmail.com>
2021-05-31 16:14:35 +0300

    Added authReducer and wrapped app to provider

--- backend/client/src/App.js
+++ backend/client/src/App.js
@@ -1,28 +1,21 @@
 import React from 'react'
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
-import { Admin, Resource } from 'react-admin';
-import jsonServerProvider from 'ra-data-json-server';
-import Landing from '../src/landing/landing'
-import {UsersList} from './components/users'
-import {TicketsList} from './components/Tickets'
-import DashBoard from './components/Dashboard'
-import {Analytics} from './components/Analytics'
-import {Categories} from './components/Categories'
-import {Status} from './components/Statuses' 
-
+import {Provider} from 'react-redux'
+import store from '../src/redux/store'
 import Register from '../src/authentication/register/Register'
 import LogIn from '../src/authentication/login/LogIn'
 
 
 const App = () => (
+    <Provider store={store}>
     <Router>
     <Route exact path="/" component={Landing}/>
     <Route exact path="/register" component={Register}/>
     <Route exact path="/LogIn" component={LogIn}/>
-
     </Router>
+    </Provider>
+    
     
 );
 
--- backend/client/src/redux/Reducers/authReducer.js
+++ backend/client/src/redux/Reducers/authReducer.js
@@ -1,1 +1,30 @@
-im
+import {SET_CURRENT_USER, USER_LOADING} from '../Actiontypes'
+
+import isEmpty from 'is-empty'
+
+const initialstate = {
+    isAuthenticated: false,
+    user: {},
+    loading: true
+}
+
+const authReducer = (state = initialstate, action) => {
+    switch(action.type){
+        case SET_CURRENT_USER:
+            return{
+                ...state,
+                isAuthenticated: !isEmpty(action.payload),
+                user: action.payload
+
+            }
+            case USER_LOADING:
+                return {
+                    ...state,
+                    loading: true
+                }
+    }
+     
+
+}
+
+}
