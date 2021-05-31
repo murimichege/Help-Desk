0735c0dc987d684b89b525342b1943bacb5ad3ae
murimichege <chegeeddie@gmail.com>
2021-05-31 16:23:26 +0300

    Added the error reducer

---
+++ backend/client/src/redux/Reducers/errorReducer.js
@@ -0,0 +1,13 @@
+import {GET_ERRORS} from '../Actiontypes'
+const initialstate = {
+
+}
+const errorReducer = (state = initialstate, action) => {
+    switch(action.type){
+        case GET_ERRORS:
+            return actiion.payload
+            default: 
+            return state
+
+    }
+}
