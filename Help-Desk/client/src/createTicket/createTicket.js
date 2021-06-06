d41034cd8371295ecaad40e3da9a98612c0f6b75
murimichege <chegeeddie@gmail.com>
2021-06-06 21:56:23 +0300

    created a navigation bar

--- Help-Desk/client/src/createTicket/createTicket.js
+++ Help-Desk/client/src/createTicket/createTicket.js
@@ -1,9 +1,25 @@
 import React from 'react'
-
+import {Navbar, Nav, Container} from 'react-bootstrap'
 function createTicket() {
     return (
-        <div className = "">
-           
+        <div className = "container">
+            <div className="Navigation">
+            <Navbar collapseOnSelect fixed="top" bg="dark">
+               <Navbar.Brand href="#home">
+                   <img src="/helpdesk" width="30" height="30" className="d-inline-top align-top"/>
+                   </Navbar.Brand>
+               <Container>
+                   <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                   <Navbar.Collapse id="responsive-navbar-nav">
+                       <Nav>
+
+                       </Nav>
+                   </Navbar.Collapse>
+
+               </Container>
+           </Navbar>
+            </div>
+          
         </div>
     )
 }
